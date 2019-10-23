d4e8b26bb9ad4243ba8b1a6086f3898c2b06912b
firebingo <matthew.t.kides@gmail.com>
2019-10-23 15:06:11 -0700

    Now using asp.net middleware with gzip for response/file compression.

--- KidesServer/Startup.cs
+++ KidesServer/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
@@ -35,107 +37,69 @@ namespace KidesServer
 		{
 			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-			services.AddMvc().AddJsonOptions(opt =>
+			services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+			services.AddResponseCompression(options =>
 			{
-				opt.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-				opt.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+				options.Providers.Add<GzipCompressionProvider>();
+				options.EnableForHttps = true;
+				options.MimeTypes = new string[]
+				{
+					"text/css",
+					"text/html",
+					"text/json",
+					"text/plain",
+					"text/xml",
+					"application/javascript",
+					"application/x-javascript",
+					"application/json"
+				};
 			});
 
-			try
-			{
-				services.AddAuthentication(AuthInfo.LoginAuthScheme)
-					.AddCookie(options =>
+			services.AddAuthentication(AuthInfo.LoginAuthScheme)
+				.AddCookie(options =>
+				{
+					options.Events.OnRedirectToLogin = (context) =>
 					{
-						options.Events.OnRedirectToLogin = (context) =>
+						context.Response.StatusCode = 401;
+						return Task.CompletedTask;
+					};
+					options.Events.OnValidatePrincipal = async (context) =>
+					{
+						var username = context.Principal?.Identity?.Name ?? string.Empty;
+						FileControllerPerson user = null;
+						if (!string.IsNullOrWhiteSpace(username) && AppConfig.Config.FileAccess.People.ContainsKey(username.ToLowerInvariant()))
+							user = AppConfig.Config.FileAccess.People[username.ToLowerInvariant()];
+
+						if (user == null && !AppConfig.Config.FileAccess.People.ContainsKey("anon"))
 						{
-							context.Response.StatusCode = 401;
-							return Task.CompletedTask;
-						};
-						options.Events.OnValidatePrincipal = async (context) =>
-						{
-							var username = context.Principal?.Identity?.Name ?? string.Empty;
-							FileControllerPerson user = null;
-							if (!string.IsNullOrWhiteSpace(username) && AppConfig.Config.FileAccess.People.ContainsKey(username.ToLowerInvariant()))
-								user = AppConfig.Config.FileAccess.People[username.ToLowerInvariant()];
-
-							if (user == null && !AppConfig.Config.FileAccess.People.ContainsKey("anon"))
-							{
-								context.RejectPrincipal();
-								await context.HttpContext.SignOutAsync(AuthInfo.LoginAuthScheme);
-								return;
-							}
-							else if (user == null)
-								user = AppConfig.Config.FileAccess.People["anon"];
-
-							if (user.Disabled)
-							{
-								context.RejectPrincipal();
-								await context.HttpContext.SignOutAsync(AuthInfo.LoginAuthScheme);
-								return;
-							}
-
+							context.RejectPrincipal();
+							await context.HttpContext.SignOutAsync(AuthInfo.LoginAuthScheme);
 							return;
-						};
-						options.SlidingExpiration = true;
-						options.ExpireTimeSpan = TimeSpan.FromDays(5);
-						options.Cookie.Name = AuthInfo.CookieName;
-					});
-			}
-			catch(Exception ex)
-			{
+						}
+						else if (user == null)
+							user = AppConfig.Config.FileAccess.People["anon"];
 
-			}
+						if (user.Disabled)
+						{
+							context.RejectPrincipal();
+							await context.HttpContext.SignOutAsync(AuthInfo.LoginAuthScheme);
+							return;
+						}
 
-			//services
-			//	.AddAuthentication(BasicAuthenticationDefaults.AuthenticationScheme)
-			//	.AddBasicAuthentication(options =>
-			//	{
-			//		options.Realm = "KidesServer";
-			//		options.Events = new BasicAuthenticationEvents
-			//		{
-			//			OnValidatePrincipal = context =>
-			//			{
-			//				try
-			//				{
-			//					FileControllerPerson user = null;
-			//					if (!string.IsNullOrWhiteSpace(context.UserName) && AppConfig.Config.FileAccess.People.ContainsKey(context.UserName.ToLowerInvariant()))
-			//						user = AppConfig.Config.FileAccess.People[context.UserName.ToLowerInvariant()];
-			//					if (!AppConfig.Config.FileAccess.People.ContainsKey("anon"))
-			//					{
-			//						context.AuthenticationFailMessage = "Authentication failed.";
-			//						return Task.CompletedTask;
-			//					}
-			//					if (user == null)
-			//						user = AppConfig.Config.FileAccess.People["anon"];
-			//
-			//					if (user != null && user.CheckPassword(context.Password))
-			//					{
-			//						var claims = new List<Claim>
-			//					{
-			//						new Claim(ClaimTypes.Name,
-			//								  string.IsNullOrWhiteSpace(context.UserName) ? "anon" : context.UserName,
-			//								  context.Options.ClaimsIssuer)
-			//					};
-			//
-			//						var principal = new ClaimsPrincipal(new ClaimsIdentity(claims, BasicAuthenticationDefaults.AuthenticationScheme));
-			//						context.Principal = principal;
-			//
-			//						return Task.CompletedTask;
-			//					}
-			//				}
-			//				catch (Exception ex)
-			//				{
-			//					ErrorLog.WriteError(ex);
-			//					context.AuthenticationFailMessage = "Authentication failed.";
-			//					return Task.CompletedTask;
-			//				}
-			//
-			//				context.AuthenticationFailMessage = "Authentication failed.";
-			//				return Task.CompletedTask;
-			//			}
-			//		};
-			//	});
+						return;
+					};
+					options.SlidingExpiration = true;
+					options.ExpireTimeSpan = TimeSpan.FromDays(5);
+					options.Cookie.Name = AuthInfo.CookieName;
+				});
+
+			services.AddMvc()
+				.SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+				.AddJsonOptions(opt =>
+				{
+					opt.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+					opt.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+				});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -155,6 +119,7 @@ namespace KidesServer
 			Directory.CreateDirectory($"{AppDomain.CurrentDomain.GetData("DataDirectory").ToString()}\\Temp");
 
 			app.UseHttpsRedirection();
+			app.UseResponseCompression();
 			app.UseStaticFiles();
 			app.UseCookiePolicy(new CookiePolicyOptions()
 			{
